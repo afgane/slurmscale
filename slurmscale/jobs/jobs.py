1bd344a3ccda43f4ac1d4b94b1a18fc816c9b6ae
Enis Afgan <afgane@gmail.com>
2017-03-31 12:43:31 -0400

    Add ability to filter job list by job state

--- slurmscale/jobs/jobs.py
+++ slurmscale/jobs/jobs.py
@@ -12,7 +12,26 @@ class Jobs(object):
         """Fetch fresh data."""
         return pyslurm.job().get()
 
-    def list(self):
-        """List the current jobs on the cluster."""
+    def list(self, states=None):
+        """
+        List the current jobs on the cluster.
+
+        :type states: List of ``str``
+        :param states: Filter jobs in the given states. Available states are
+                      ``PENDING``, ``RUNNING``, ``CANCELLED``, ``CONFIGURING``,
+                      ``COMPLETING``, ``COMPLETED``, ``FAILED``, ``TIMEOUT``,
+                      ``PREEMPTED``, ``NODE_FAIL`` and ``SPECIAL_EXIT``.
+
+        :rtype: List of ``Job``
+        :return: A list of current cluster jobs, possibly filtered by supplied
+                 states.
+        """
         current_jobs = self._jobs
-        return [Job(current_jobs[j]) for j in current_jobs]
+        jobs = []
+        if states:
+            for i in current_jobs:
+                if current_jobs[i].get('job_state', '') in states:
+                    jobs.append(Job(current_jobs[i]))
+        else:
+            jobs = [Job(current_jobs[j]) for j in current_jobs]
+        return jobs
