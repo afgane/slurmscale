05b27e73bc419f0adb69ad28f9e8c629d28fff87
Enis Afgan <afgane@gmail.com>
2017-03-31 18:18:07 -0400

    Add several job properties

--- slurmscale/jobs/job.py
+++ slurmscale/jobs/job.py
@@ -33,13 +33,51 @@ class Job(object):
                  ``CONFIGURING``, ``COMPLETING``, ``COMPLETED``, ``FAILED``,
                  ``TIMEOUT``, ``PREEMPTED``, ``NODE_FAIL`` and ``SPECIAL_EXIT``
         """
-        return pyslurm.job().find_id(str(self.id))[0].get('job_state')
+        self.update()  # Always get fresh data
+        return self._job.get('job_state')
+
+    @property
+    def eligible_time(self):
+        """
+        Time when the job became eligible to run. Applicable for PENDING jobs.
+
+        :rtype: ``int``
+        :return: Eligible time as a Unix timestamp. Return 0 if not available.
+        """
+        return self._job.get('eligible_time', 0)
+
+    @property
+    def run_time(self):
+        """
+        Time the job has taken to run. Valid jobs only.
+
+        :rtype: ``int``
+        :return: Runtime of the job.
+        """
+        self.update()  # Always get fresh data
+        return self._job.get('run_time', 0)
+
+    @property
+    def state_reason(self):
+        """
+        The reason the job is in current state.
+
+        :rtype: ``str``
+        :return: One of the available Slurm reasons. See JOB REASON CODES on
+                 https://slurm.schedmd.com/squeue.html
+        """
+        sr = self._job.get('state_reason')
+        return None if sr == 'None' else sr  # Map string "None" to None
+
+    def update(self):
+        """Refresh info about the job by querying the scheduler."""
+        self._job = pyslurm.job().find_id(str(self.id))[0]
 
     def show(self):
         """
         Show complete details about this job.
 
-        *Note* that this method exists primarily for informational purposes.
+        *Note* that this method exists primarily for informational purposes;
         individual job fields should be accessed through the object's
         properties.
 
